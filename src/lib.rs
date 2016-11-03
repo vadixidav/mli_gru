4df1fdbafb58d5fcf1cd58582012671b8d8670a3
Geordon Worley <vadixidav@gmail.com>
2016-11-03 17:13:07 -0400

    add crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#![feature(plugin)]
+#![feature(proc_macro)]
+
+#[macro_use]
+extern crate serde_derive;
+extern crate rand;
+extern crate num;
+extern crate mli;
+
+
